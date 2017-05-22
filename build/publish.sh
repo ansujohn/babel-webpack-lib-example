2a839f02ced696140d3f990d0583d66e4a5f6d02
Ansu John <ansu.john@concur.com>
2017-05-22 09:02:03 +0100

    Version and release

--- build/publish.sh
+++ build/publish.sh
@@ -2,14 +2,12 @@
 
 set -o errexit
 
-git config --global user.email "nui-build@github.com"
-git config --global user.name "nui-build"
+git config --global user.email "ansu.john@concur.com"
+git config --global user.name "ansu.john"
 
 # update the package verion and commit to the git repository
 npm run std-version
 
-# use the key that Shippable uses to connect to GitHub
-# ssh-agent bash -c "ssh-add ~/.ssh/project_rsa; git push --tags origin ${CIRCLE_BRANCH}"
-git push --tags origin ${CIRCLE_BRANCH}
+git push --follow-tags origin master
 
 npm publish
