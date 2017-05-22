54312d7a43cea483d2f30001d56ff7e67125f302
Ansu John <ansujohn@users.noreply.github.com>
2017-05-22 09:05:34 +0100

    Version and release (#1)
    * Version and release
    
    * Version and release

---
+++ build/publish.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -o errexit
+
+git config --global user.email "ansu.john@concur.com"
+git config --global user.name "ansu.john"
+
+# update the package verion and commit to the git repository
+npm run std-version
+
+git push --follow-tags origin master
+
+npm publish
