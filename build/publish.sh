994728db2c1edf5ab0e956b75fc25410a834c20d
ansu.john <ansu.john@concur.com>
2017-06-05 13:59:34 +0100

    commenting out the user email

--- build/publish.sh
+++ build/publish.sh
@@ -2,8 +2,8 @@
 
 set -o errexit
 
-git config --global user.email "ansu.john@concur.com"
-git config --global user.name "ansu.john"
+# git config --global user.email "ansu.john@concur.com"
+# git config --global user.name "ansu.john"
 
 # update the package verion and commit to the git repository
 npm run std-version
