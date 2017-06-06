ef2bc3cbd90d2087009aa1437864ebef1fd69acc
ansu.john <ansu.john@concur.com>
2017-06-06 22:07:57 +0100

    perimeter

--- src/lib/perimeter.js
+++ src/lib/perimeter.js
@@ -3,9 +3,11 @@ import * as constants from './../constants/constants';
 const square = (width) => {
     return width * 4;
 };
+
 const rectangle = (length, width) => {
     return 2 * length + 2 * width;
 };
+
 const circle = (radius) => {
     return Math.round(2 * constants.PI * radius * 100) / 100;
 };
