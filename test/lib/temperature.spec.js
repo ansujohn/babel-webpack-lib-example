32b88878f9c9f19172807a7cb0a2295a98dcc952
ansu.john <ansu.john@concur.com>
2017-06-06 11:12:21 +0100

    Added test case

--- test/lib/temperature.spec.js
+++ test/lib/temperature.spec.js
@@ -10,6 +10,10 @@ describe('Temperature tests', function() {
         expect(temperature.celsiusToFarenheit(37)).to.be.equal(98.6);
     });
 
+    it('Should return the temperature in Fareheit', function() {
+        expect(temperature.celsiusToFarenheit(38.5)).to.be.equal(101.3);
+    });
+
     it('Should return the temperature in Celsius', function() {
         expect(temperature.farenheitToCelsius(98.6)).to.be.equal(37);
     });
