c246fe324f1978ada6d7bbe05c3a2392bd3006d7
Ansu John <ansujohn@users.noreply.github.com>
2017-06-16 14:31:49 +0100

    test: Add test and see the version (#28)

--- build/publish.sh
+++ build/publish.sh
@@ -5,11 +5,6 @@ set -o errexit
 # update the package version and commit to the git repository
 npm run std-version
 
-git push --follow-tags origin master
+git push --follow-tags origin develop
 
 npm publish
-
-git checkout develop
-git pull origin develop
-git pull --no-edit origin master
-git push
